3977a1e1f62bfc92bc11fb2cfc0005121b6bd164
Adesh Singh Rathore <72265093+AdeshSingh7@users.noreply.github.com>
2023-01-05 20:15:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-import os
-
-tools = 'nano vim git wget figlet toilet curl zip apache2 net-tools php nmap ruby libminizip1 libgtkglext1 python python3 python3-pip python3-tk python3-dev'
-module = 'ansible DateTime Flask Pillow PyAutoGUI pyfiglet requests tbomb termcolor urllib3 colorama'
-ngrok = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
-ngrok_key = '< Auth token >'
-anydesk = 'https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb'
-visualstudio = 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+import os, json
 
 main_menu_items = [
     "[a] Update & Install tools:- This option will update the system and install a list of commonly used tools and libraries.",
@@ -21,50 +14,60 @@ main_menu_items = [
     "[q] Quit:- This option will exit the program.",
 ]
 
+# open the config.json file in write mode
+with open('config.json', 'r') as config_file:
+  # write the data to the file as JSON
+  config_data = json.load(config_file)
+  tools = config_data['tools']
+  module = config_data['module']
+  ngrok = config_data['ngrok']
+  ngrok_key = config_data['ngrok_key']
+  anydesk = config_data['anydesk']
+  visualstudio = config_data['visualstudio']
+
 def Clean():
-    os.system('sudo apt -y autoremove')
-    os.system('sudo apt -y autoclean')
+    os.system(f'sudo apt -y autoremove && sudo apt -y autoclean')
 
-def Update():
-    os.system('sudo apt -y update')
-    os.system('sudo apt -y install '+tools)
-    os.system('sudo apt -y --fix-broken install')
-    os.system('sudo gem install lolcat')
-    os.system('pip install '+module)
+def Update(tools, module):
+    os.system(f'sudo apt -y update')
+    os.system(f'sudo apt -y install {tools}')
+    os.system(f'sudo apt -y --fix-broken install')
+    os.system(f'sudo gem install lolcat')
+    os.system(f'pip install {module}')
 
-def Ngrok():
-    os.system('wget '+ngrok)
-    os.system('unzip ngrok-stable-linux-amd64.zip')
-    os.system('rm -fr ngrok-stable-linux-amd64.zip')
-    os.system('sudo mv ngrok /usr/bin/')
-    os.system('ngrok '+ngrok_key)
-    os.system('sudo ngrok '+ngrok_key)
+def Ngrok(ngrok, ngrok_key):
+    os.system(f'wget {ngrok}')
+    os.system(f'unzip ngrok*.zip')
+    os.system(f'rm -fr ngrok*.zip')
+    os.system(f'sudo mv ngrok /usr/bin/')
+    os.system(f'ngrok {ngrok_key}')
+    os.system(f'sudo ngrok {ngrok_key}')
 
 def Wine():
-    os.system('sudo dpkg --add-architecture i386')
-    os.system('wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -')
-    os.system("sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'")
-    os.system('sudo apt -y update')
-    os.system('sudo apt install --install-recommends winehq-stable')
-    os.system('wine --version')
+    os.system(f'sudo dpkg --add-architecture i386')
+    os.system(f'wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -')
+    os.system(f"sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'")
+    os.system(f'sudo apt -y update')
+    os.system(f'sudo apt install --install-recommends winehq-stable')
+    os.system(f'wine --version')
 
-def Anydesk():
-    os.system('wget '+anydesk)
-    os.system('sudo dpkg -i *.deb')
-    os.system('sudo rm -frv *.deb')
+def Anydesk(anydesk):
+    os.system(f'wget {anydesk}')
+    os.system(f'sudo dpkg -i *.deb')
+    os.system(f'sudo rm -frv *.deb')
 
 def Whatsapp():
-    os.system('sudo apt -y install snapd')
-    os.system('sudo snap install whatsapp-for-linux')
+    os.system(f'sudo apt -y install snapd')
+    os.system(f'sudo snap install whatsapp-for-linux')
 
-def VisualStudio():
-    os.system('wget '+visualstudio)
-    os.system('sudo dpkg -i *.deb')
-    os.system('sudo rm -frv *.deb')
+def VisualStudio(visualstudio):
+    os.system(f'wget {visualstudio}')
+    os.system(f'sudo dpkg -i *.deb')
+    os.system(f'sudo rm -frv *.deb')
 
 def Slack():
-    os.system('sudo apt -y install snapd')
-    os.system('sudo snap install slack')
+    os.system(f'sudo apt -y install snapd')
+    os.system(f'sudo snap install slack')
 
 def Tag_Sync():
-    os.system('git clone https://github.com/AdeshSingh7/tag-sync.git')
+    os.system(f'git clone https://github.com/AdeshSingh7/tag-sync.git')
